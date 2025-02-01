a692fb36f6b04112070150acb1394819d99f4d3f
LazebnyiDanylo <daniillazebnuy124@gmail.com>
2025-02-01 17:20:32 +0200

    do lesson

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -6,6 +6,15 @@
 
 // Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
 // перетворіть значення на число за допомогою Number().
+
+// const number = Number(prompt('Enter a number'));
+// if (number === 10) {
+//   alert('Correct!');
+// } else {
+//   alert('Incorrect!');
+// }
+// console.log(typeof number);
+
 // ----------------------------------------------------
 // У змінній min лежить випадкове число від 0 до 59.
 // Визначте, в яку чверть години потрапляє
@@ -14,6 +23,18 @@
 // "10 входить в першу чверть"
 
 // const min = Math.floor(Math.random() * (59 - 0) + 0);
+// if (min < 15) {
+//   alert(`${min} enters the first quarter`);
+// } else if (min < 30) {
+//   alert(`${min} enters the second quarter`);
+// } else if (min < 45) {
+//   alert(`${min} enters the third quarter`);
+// } else if (min < 60) {
+//   alert(`${min} enters the fourth quarter`);
+// } else {
+//     alert('Incorrect number')
+// }
+
 // ---------------------------------------------------------
 // Змінна num може набувати 4 значення: '1', '2', '3' або '4'
 // (запитуй це значення у користувача через prompt).
@@ -24,12 +45,44 @@
 // введе в prompt щось інше. В такому випадку result має набувати значення:
 // "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
 // Значення змінної result виведіть в консоль.
+
+// const num = Number(prompt('Запишіть значення від 1 до 4'));
+// let result = '';
+// switch (num) {
+//   case 1:
+//     result = 'Зима';
+//     break;
+//   case 2:
+//     result = 'Весна';
+//     break;
+//   case 3:
+//     result = 'Літо';
+//     break;
+//   case 4:
+//     result = 'Осінь';
+//     break;
+//   default:
+//     result = 'Вибачте, але ви маєте ввести значення від 1 до 4 включно';
+// }
+// console.log(result);
+
 // --------------------------------------------------------------
 // Отримуйте від користувача число (кількість хвилин) через prompt
 // і виводьте у консоль рядок у форматі годин та хвилин.
 // Приклад: користувач вводить в prompt '70' -> в консолі відобразиться '01:10'.
 // Корисне посилання для відображення годин та хвилин у потрібному форматі ('01' замість '1'):
 // <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples>
+
+// const num = Number(prompt('Введіть кількість хвилин'));
+// function formatTime(totalMinutes) {
+//   const hours = Math.floor(totalMinutes / 60);
+//   const modifiedHours = String(hours).padStart(2, '0');
+//   const minutes = totalMinutes % 60;
+//   const modifiedMinutes = String(minutes).padStart(2, '0');
+//   return `${modifiedHours}:${modifiedMinutes}`;
+// }
+// console.log(formatTime(num));
+
 // ----------------------------------------------------------------------------------------------
 // Напишіть код, який запитуватиме у користувача
 // логін за допомогою prompt і виводить результат в консоль браузера
@@ -54,6 +107,19 @@
 // всі числа від max до min за спаданням.
 // Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
 // і поверніть її з функції.
+
+// function getNumber(min, max) {
+//   let sum = 0;
+//   for (let i = max; i >= min; i -= 1) {
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//     console.log(i);
+//   }
+//   return sum;
+// }
+// console.log(getNumber(0, 6));
+
 // ------------------------------------------------------------------------------------------
 // Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
 // Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
